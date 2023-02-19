47b590750cd2b502dcd6be30d6cd2e942b951d28
sham-nt <90405823+sham-nt@users.noreply.github.com>
2023-02-19 13:01:08 +0530

    Basic Tkinter UI created

--- main.py
+++ main.py
@@ -1,18 +1,94 @@
+'''
+Article Summarizer App
+Created by - @sham-nt(Shamanth Kashyap)
+
+'''
 import nltk
+import tkinter as tk
 from textblob import TextBlob
 from newspaper import Article
-
 nltk.download('punkt')
 
-url = input(">")
+def summarize():
+    url = utext.get("1.0","end").strip()
+    article = Article(url)
 
-article = Article(url)
+    article.download()
+    article.parse()
+    article.nlp()
 
-article.download()
-article.parse()
-article.nlp()
+    title.config(state='normal')
+    author.config(state='normal')
+    publication.config(state='normal')
+    summary.config(state='normal')
+    sentiment.config(state='normal')
+    
+    title.delete('1.0','end')
+    author.delete('1.0','end')
+    publication.delete('1.0','end')
+    summary.delete('1.0','end')
+    sentiment.delete('1.0','end')
+    
+    title.insert('1.0',article.title)
+    author.insert('1.0',article.authors)
+    publication.insert('1.0',article.publish_date)
+    summary.insert('1.0',article.summary.replace('\n',''))
 
-print(f"Title: {article.title}")
-print(f"Authors: {article.authors}")
-print(f"Publication Date: {article.publish_date}")
-print(f"Summary: {article.summary}")
+    analysis = TextBlob(article.text)
+    
+    sentiment.delete('1.0','end')
+    sentiment.insert('1.0',f'Polarity: {analysis.polarity},Sentiment: {"positive" if analysis.polarity > 0 else "negative" if analysis.polarity < 0 else "neutral"}')
+
+
+
+#Tkinter implementation
+root = tk.Tk()
+root.title("News Summarizer")
+root.geometry("1200x600")
+
+tlabel = tk.Label(root,text="Title")
+tlabel.pack()
+
+title = tk.Text(root,height=1,width=140)
+title.config(state='disabled',bg='#dddddd')
+title.pack()
+
+alabel = tk.Label(root,text='Author')
+alabel.pack()
+
+author = tk.Text(root,height=1,width=140)
+author.config(state='disabled',bg='#dddddd')
+author.pack()
+
+plabel = tk.Label(root,text='Publishing Data')
+plabel.pack()
+
+publication = tk.Text(root,height=1,width=140)
+publication.config(state='disabled',bg='#dddddd')
+publication.pack()
+
+slabel = tk.Label(root,text='Summary')
+slabel.pack()
+
+summary = tk.Text(root,height=20,width=140)
+summary.config(state='disabled',bg='#dddddd')
+summary.pack()
+
+selabel = tk.Label(root,text='Sentiment')
+selabel.pack()
+
+sentiment = tk.Text(root,height=1,width=140)
+sentiment.config(state='disabled',bg='#dddddd')
+sentiment.pack()
+
+ulabel = tk.Label(root,text='URL')
+ulabel.pack()
+
+utext = tk.Text(root,height=1,width=140)
+utext.config(bg='#dddddd')
+utext.pack()
+
+btn = tk.Button(root,text = "Summarize",command = summarize)
+btn.pack()
+
+root.mainloop()
